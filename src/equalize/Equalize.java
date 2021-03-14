6974199c53634fda83b06ae5d0546be4d5581587
牟钰 <18373019@buaa.edu.cn>
2021-03-14 20:40:59 +0800

    1.0.1 - Support TYPE_BYTE_GRAY

--- src/equalize/Equalize.java
+++ src/equalize/Equalize.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Equalize {
     private BufferedImage grayImage;
@@ -73,7 +75,16 @@ public class Equalize {
 
     // only support TYPE_3BYTE_BGR now
     private BufferedImage getGrayImage(BufferedImage image) throws TypeNotSupportedException {
-        if (image.getType() != BufferedImage.TYPE_3BYTE_BGR) {
+        ArrayList<Integer> supportedType = new ArrayList<>(
+                Arrays.asList(BufferedImage.TYPE_3BYTE_BGR, BufferedImage.TYPE_BYTE_GRAY)
+        );
+        boolean ok = false;
+        for (Integer type: supportedType) {
+            if (image.getType() == type) {
+                ok = true;
+            }
+        }
+        if (!ok) {
             throw new TypeNotSupportedException(image.getType());
         }
 
@@ -84,13 +95,22 @@ public class Equalize {
                 coloredPixels, 0, imageWidth);
 
         for (int i = 0; i < coloredPixels.length; i++) {
-            int bgr = coloredPixels[i];
-            int blue_part = bgr & 0xff0000 >> 16;
-            int green_part = bgr & 0x00ff00 >> 8;
-            int red_part = bgr & 0x0000ff;
-            int gray = (int) (0.299 * red_part + 0.587 * green_part + 0.114 * blue_part);
-            int newBgr = gray << 16 | gray << 8 | gray;
-            rawPixels[i] = newBgr;
+            int val = 0;
+            if (image.getType() == BufferedImage.TYPE_3BYTE_BGR) {
+                int bgr = coloredPixels[i];
+                int blue_part = bgr & 0xff0000 >> 16;
+                int green_part = bgr & 0x00ff00 >> 8;
+                int red_part = bgr & 0x0000ff;
+                int gray = (int) (0.299 * red_part + 0.587 * green_part + 0.114 * blue_part);
+                val = gray << 16 | gray << 8 | gray;
+            } else if (image.getType() == BufferedImage.TYPE_BYTE_GRAY) {
+                int bgr = coloredPixels[i];
+                int gray = bgr & 0xff0000 >> 16;
+                assert((bgr & 0x00ff00 >> 8) == gray);
+                assert((bgr & 0x0000ff) == gray);
+                val = gray << 16 | gray << 8 | gray;
+            }
+            rawPixels[i] = val;
         }
 
         BufferedImage result = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_BYTE_GRAY);
