e5e0065e7bb079dccdb686bca3e33520d73be624
牟钰 <18373019@buaa.edu.cn>
2021-03-18 22:03:31 +0800

    2.0.1 JUnit test for mapping

---
+++ src/transform/LinearTransformMappingTest.java
@@ -0,0 +1,27 @@
+package transform;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinearTransformMappingTest {
+
+    @Test
+    public void linearTransformMapping() {
+        assert(LinearTransformMapping.linearTransformMapping(0, 255, 0, 255, 0) == 0);
+        assert(LinearTransformMapping.linearTransformMapping(0, 255, 0, 255, 255) == 255);
+        assert(LinearTransformMapping.linearTransformMapping(1, 255, 0, 255, 0) == 0);
+        assert(LinearTransformMapping.linearTransformMapping(1, 255, 0, 255, 255) == 255);
+
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 100, 200, 50) == 100);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 100, 200, 0) == 100);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 100, 200, 100) == 200);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 100, 200, 255) == 200);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 100, 200, 75) == 150);
+
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 0, 255, 0) == 0);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 0, 255, 50) == 0);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 0, 255, 255) == 255);
+        assert(LinearTransformMapping.linearTransformMapping(50, 100, 0, 255, 100) == 255);
+    }
+}
